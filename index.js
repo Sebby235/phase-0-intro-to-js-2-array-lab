0ce63cf75569718d2f47c195d3cda0cd547fa306
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-04-18 12:18:58 -0500

    Arrays

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph) {
+cats.push("Ralph");
+}
+function destructivelyPrependCat(Gizmo) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(Ralph) {
+    cats.pop("Ralph")
+}
+function destructivelyRemoveFirstCat(Bob) {
+    cats.shift("Bob");
+}
+function appendCat(Broom) {
+    const cats = ["Milo", "Otis", "Garfield"];
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+function prependCat(Arnold) {
+    const newNewCats = ["Arnold", ...cats];
+    return newNewCats;
+}
+function removeLastCat() {
+    const catsCopy = cats.slice();
+    catsCopy.pop();
+    return catsCopy;
+}
+function removeFirstCat() {
+    const catsNewCopy = cats.slice ();
+    catsNewCopy.shift();
+    return catsNewCopy;
+}
